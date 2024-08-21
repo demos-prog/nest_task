bb49a979a49c975070e282e2771e3d65f06f65bf
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-08-21 09:45:28 +0300

    Revert "deleting secret"
    This reverts commit 453c1f65ccd812037dfa79904eed082792061bc2.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '../guards/auth.guard';
 
@@ -10,6 +11,7 @@ import { AuthGuard } from '../guards/auth.guard';
   imports: [
     JwtModule.register({
       global: true,
+      secret: jwtConstants.secret,
       signOptions: { expiresIn: '1d' },
     }),
   ],
--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from '../auth/constants';
 import { IS_PUBLIC_KEY } from '../auth/decorator.factory';
 import { Request } from 'express';
 
@@ -31,7 +32,9 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(token);
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
